5476ff528eb2a77045263a1badfea1a7de9a5784
isabella2810 <35439321+isabella2810@users.noreply.github.com>
2018-06-17 21:57:53 -0400

    Update RotationArray.java

--- RotationArray.java
+++ RotationArray.java
@@ -7,15 +7,19 @@ Questions involving this array:
 1. find the index of rotation. In our case, it is 3 because arr[3] = 0
 2. given a number t, return the index of t in this array or -1 if t is not in the array
 
-Q1 is solved by the function rotation_recursive() and/or rotation_iterative(). Both can solve the problem correctly; the difference is that one is implemented with recursion while the other one uses loop. Our implementation applies to the cases when:
-1) the array has duplicates (i.e., non-strictly monototically ascending with rotation);
-2) the array is fully sorted (e.g., [0,1,2,3,4,5,6]. The function will return 0 then).
-
-Q2 is solved by the function rotation_and_find_recursive() and/or rotation_and_find_iterative(). Again, both can solve the problem correctly; the difference is that one is implemented with recursion while the other one uses loop. 
+Q1 is solved by the function rotation_recursive() and/or rotation_iterative(). Both can solve the problem correctly; 
+the difference is that one is implemented with recursion while the other one uses loop. 
 Our implementation applies to the cases when:
-1) the array has duplicates (i.e., non-strictly monototically ascending with rotation);
-2) the array is fully sorted;
-3) the target number t is not in the array. [return -1 to indicate that].
+    1) the array has duplicates (i.e., non-strictly monototically ascending with rotation);
+    2) the array is fully sorted (e.g., [0,1,2,3,4,5,6]. The function will return 0 then).
+
+Q2 is solved by the function rotation_and_find_recursive() and/or rotation_and_find_iterative(). 
+Again, both can solve the problem correctly; 
+the difference is that one is implemented with recursion while the other one uses loop. 
+Our implementation applies to the cases when:
+    1) the array has duplicates (i.e., non-strictly monototically ascending with rotation);
+    2) the array is fully sorted;
+    3) the target number t is not in the array. [return -1 to indicate that].
 
 */
 
